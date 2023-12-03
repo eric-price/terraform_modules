8ac99561569a2bd4d71ee6dbde85188d902b66b5
eric-price <eric2025@gmail.com>
2023-12-03 13:09:59 -0800

    move karpenter dir

--- karpenter/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "cluster_name" {}
-variable "cluster_endpoint" {}
-variable "env" {}
-variable "region" {}
-variable "irsa_oidc_provider_arn" {}
-variable "eks_node_role_arn" {}
-variable "karpenter_version" {}
